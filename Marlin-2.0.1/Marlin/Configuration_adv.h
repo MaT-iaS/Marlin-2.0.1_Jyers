3cabeaafb46a598bda26576e02455b0314ca8436
Mat-iaS <mts1788@gmail.com>
2023-07-14 22:16:53 -0300

    module laser configuration
    + enabled SOFT_PWM_SCALE 4
    + enabled SOFT_PWM_DITHER
    + enabled  LASER_SYNCHRONOUS_M106_M107
    - disabled FAN_MIN_PWM

--- Marlin-2.0.1/Marlin/Configuration_adv.h
+++ Marlin-2.0.1/Marlin/Configuration_adv.h
@@ -535,7 +535,7 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-#define FAN_MIN_PWM 50
+// #define FAN_MIN_PWM 50
 //#define FAN_MAX_PWM 128
 
 /**
@@ -3506,7 +3506,7 @@
  *
  * NOTE: This option sacrifices some cooling fan speed options.
  */
-//#define LASER_SYNCHRONOUS_M106_M107
+#define LASER_SYNCHRONOUS_M106_M107
 
 /**
  * Coolant Control
